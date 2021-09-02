053ae4dfd7e6c9e394708b77c24c7f0ef8310702
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-02 18:31:35 +0530

    adding EMPCHECK

---
+++ EmployeeWageCompute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWP
+{
+    class EmployeeWageCompute
+    {
+        static void Main(string[] args)
+        {
+            //constants
+            int IS_PRESENT = 1; 
+
+            //Predefined Random class for generating random values
+            Random random = new Random();
+            
+            //Next method() 0-initial value,2-number of elemnets from 0
+            int empCheck = random.Next(0, 2);
+
+            //Selection statement
+            if (empCheck == IS_PRESENT)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is Absent");
+            Console.Read();
+        }
+    }
+}
+
