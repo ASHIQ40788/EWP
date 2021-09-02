7204a4e65dbc81d2d69995cfcd40c6f8270cfd65
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-02 19:18:33 +0530

    ADDED: Calculate the wage

--- EmployeeWageCompute.cs
+++ EmployeeWageCompute.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EWP
+namespace EmployeeWage
 {
     class EmployeeWageCompute
     {
         static void Main(string[] args)
         {
             //constants
-            int IS_PRESENT = 1; 
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
 
             //Predefined Random class for generating random values
             Random random = new Random();
@@ -20,10 +25,17 @@ namespace EWP
             int empCheck = random.Next(0, 2);
 
             //Selection statement
-            if (empCheck == IS_PRESENT)
-                Console.WriteLine("Employee is present");
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+              
             else
-                Console.WriteLine("Employee is Absent");
+            {
+                empHrs = 8;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Emp Wage :"+empWage);
             Console.Read();
         }
     }
