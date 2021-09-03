63cb16314c317237aee79d16e9360aa0076b2780
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-03 17:07:57 +0530

    UC6-EMPLOYEE WAGE

--- EmployeeWageCompute.cs
+++ EmployeeWageCompute.cs
@@ -13,16 +13,17 @@ namespace EmployeeWage
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH=100;
         static void Main(string[] args)
         {
             //local Variables
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
             
-            
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
-            {
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            { 
+                totalWorkingDays++;
                 //Predefined Random class for generating random values
               Random random = new Random();
 
@@ -42,12 +43,17 @@ namespace EmployeeWage
                   empHrs = 0;
                   break;
                }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                Console.WriteLine($"Day {day} EmployeeWage is = {empWage}");
-                 totalEmpWage += empWage;
+          
+                totalEmpHrs += empHrs ;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
+                //Console.WriteLine($"Day {day} EmployeeWage is = {empWage}");
+                 //totalEmpWage += empWage;
             }
-            Console.ReadKey();
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("total emp Wage:" +totalEmpWage);
+            Console.ReadKey ();
         }
+     
     }
 }
 
