a277781fad086e2285860f776d9bccf6a30b0f78
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-03 13:19:51 +0530

    EMPLOYEE WAGE

--- EmployeeWageCompute.cs
+++ EmployeeWageCompute.cs
@@ -11,31 +11,36 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             //constants
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
-
+            
             //Variables
             int empHrs = 0;
             int empWage = 0;
 
             //Predefined Random class for generating random values
             Random random = new Random();
-            
+            //Computation
             //Next method() 0-initial value,2-number of elemnets from 0
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0,3);
 
             //Selection statement
-            if (empCheck == IS_FULL_TIME)
+            if (empCheck == IS_PART_TIME)
             {
-                empHrs = 8;
+                empHrs = 4;
             }
               
-            else
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
+            else
+            {
+                empHrs = 0;
+            }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-                Console.WriteLine("Emp Wage :"+empWage);
+            Console.WriteLine("Emp Wage :"+empWage);
             Console.Read();
         }
     }
