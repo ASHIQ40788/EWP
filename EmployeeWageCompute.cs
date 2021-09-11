77be632db875b7617ad99a68ab3061ed54cd41cb
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-11 17:31:16 +0530

    [ASHIQ] added : Employee Wage

--- EmployeeWageCompute.cs
+++ EmployeeWageCompute.cs
@@ -6,84 +6,96 @@ using System.Threading.Tasks;
 
 namespace EmployeeWage
 {
-    public class EmployeeWageCompute
+    public class EmployeeWageComputeArray
     {
         //constants
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
 
-        private string company;
-        private int empRatePerHour;
-        private int numOfWorkingDays;
-        private int maxHoursPerMonth;
-        private int totalEmpWage;
+        private int numOfCompany = 0;
+        private CompanyEmpWage[] companyEmpwageArray;
 
-        public EmployeeWageCompute(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        public EmployeeWageComputeArray()
         {
-            this.company = company;
-            this.empRatePerHour = empRatePerHour;
-            this.numOfWorkingDays = numOfWorkingDays;
-            this.maxHoursPerMonth = maxHoursPerMonth;
+            this.companyEmpwageArray = new CompanyEmpWage[5];
+        }
+
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            companyEmpwageArray[this.numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+            numOfCompany++;
         }
         public void computeEmpWage()
         {
-
-        
-            //local Variables
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-            
-            //Computation
-           while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+            for (int i = 0; i < numOfCompany; i++)
             {
-                totalWorkingDays++;
-                //Predefined Random class for generating random values
-                Random random = new Random();
-
+                companyEmpwageArray[i].setTotalEmpWage(this.computeEmpWage(this.companyEmpwageArray[i]));
+                Console.WriteLine(this.companyEmpwageArray[i].toString());
+            }
+        }
+            private int computeEmpWage(CompanyEmpWage companyEmpWage)
+            {
+                //local Variables
+                int empHrs = 0;
+                int totalEmpHrs = 0;
+                int totalWorkingDays = 0;
                 //Computation
-                //Next method() 0-initial value,2-number of elemnets from 0
-                int empCheck = random.Next(0, 3);
-                //switch selection statement-execution steps are less
-                switch (empCheck)
+                while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth  && totalWorkingDays < companyEmpWage.numOfWorkingDays)
                 {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
+                    totalWorkingDays++;
+                    //Predefined Random class for generating random values
+                    Random random = new Random();
 
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
-                //Console.WriteLine($"Day {day} EmployeeWage is = {empWage}");
-                //totalEmpWage += empWage;
+                    //Computation
+                    //Next method() 0-initial value,2-number of elemnets from 0
+                    int empCheck = random.Next(0, 3);
+                    //switch selection statement-execution steps are less
+                    switch (empCheck)
+                    {
+                        case IS_PART_TIME:
+                            empHrs = 4;
+                            break;
+                        case IS_FULL_TIME:
+                            empHrs = 8;
+                            break;
+                        default:
+                            empHrs = 0;
+                            break;
+                    }
+
+                    totalEmpHrs += empHrs;
+                    Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
+                }
+                return totalEmpHrs * companyEmpWage.empRatePerHour;
+            }
+    }
+        public class CompanyEmpWage
+        {
+            public string company;
+            public int empRatePerHour;
+            public int numOfWorkingDays;
+            public int maxHoursPerMonth;
+            public int totalEmpWage;
+            public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+            {
+                this.company = company;
+                this.empRatePerHour = empRatePerHour;
+                this.numOfWorkingDays = numOfWorkingDays;
+                this.maxHoursPerMonth = maxHoursPerMonth;
+            }
+            public void setTotalEmpWage(int totalEmpWage)
+            {
+                this.totalEmpWage = totalEmpWage;
             }
 
-            totalEmpWage = totalEmpHrs * this.empRatePerHour;
-            Console.WriteLine("total emp Wage for company:" + company + "is:" + totalEmpWage);
- 
-        }
-        public string toString()
-        {
-            return "total emp wage for company:" + this.company + "is:" + this.totalEmpWage;
-        }
-        static void Main(string[] args)
-        {
-            EmployeeWageCompute dMart = new EmployeeWageCompute("DMart", 20, 2, 10);
-            EmployeeWageCompute reliance = new EmployeeWageCompute("Reliance", 10, 4, 20);
-            dMart.computeEmpWage();
-            Console.WriteLine(dMart.toString());
-            reliance.computeEmpWage();
-            Console.WriteLine(reliance.toString());
-            Console.ReadKey();
-        }
 
-
-    }
+            public string toString()
+            {
+                return "total emp wage for company:" + this.company + "is:" + this.totalEmpWage;
+            }
+        }
+    
 }
 
+       
+        
