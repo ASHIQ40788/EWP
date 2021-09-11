588f70c0e2604e5e34771f2ea7c4e9f688f016d2
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-11 17:28:52 +0530

    [ASHIQ] added : Employee-Wage-for-Multiple-Companies

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EmployeeWageComputeArray empWageBuilder = new EmployeeWageComputeArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 20, 2, 10);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
